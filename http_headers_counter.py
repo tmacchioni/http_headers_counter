98894528668c6c9aceb487428cc4808a8e9f4983
Tommaso Macchioni <t.macchioni@gmail.com>
2020-06-02 11:30:33 +0200

    Added argument options: -f for read files, -d for read a directory, added SIGINT handler

--- http_headers_counter.py
+++ http_headers_counter.py
@@ -8,15 +8,30 @@
 # ./http_headers_counter.py
 #
 
-from scapy.all import *
+from scapy.all import sniff, load_layer
 from scapy.layers import http
 from collections import Counter
 from pathlib import Path
 import os
 import glob
+import argparse
+import signal
+import sys
+
+
+def signal_handler(sig, frame):
+    final_print()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 SCRIPT_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
-PCAPS_DIR = Path(f'{SCRIPT_DIR}/pcaps')
+PCAPS_DIR = Path(f'{SCRIPT_DIR}/pcaps') #default directory
+
+load_layer("http")
+headers_counts = Counter()
+num_of_http_pkts = 0
+
 
 # https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
 
@@ -129,68 +144,99 @@ KNOWN_HEADERS = [
 
 
 ##############################################################################################################
-# MAIN 
+# FINAL_PRINT
 
-# Create a Header Counter
-headers_counts = Counter()
+def final_print():
+    print('\n*******************************************************************')
+    print('***************************** Summary *****************************')
+    print('*******************************************************************')
+    print('\033[1m{:40}{:20}{}\033[0m'.format('HEADER', 'COUNTS', 'KNOWN'))
 
-# Create a list of pcap file names from PCAPS_DIR
-os.chdir(f'{PCAPS_DIR}')
-files_list = glob.glob('*.pcap')
+    total_headers_known = 0
+    for key,value in headers_counts.most_common():
 
-if not len(files_list):
-    print(f"Error: no pcap files was found")
-    exit()
+        headerIsKnown = False
 
-load_layer("http")
-num_of_http_pkts = 0
+        if key in KNOWN_HEADERS:
+            headerIsKnown = True
+            total_headers_known = total_headers_known + 1
 
-# Iterate the files
-for pcap_file in files_list:
+        print ('{:34}{:10}{knwon:>20}'.format(key, value, knwon = 'yes' if headerIsKnown else 'no'))
 
-    print(f'Reading file {pcap_file}')
-
-    pkts = rdpcap(pcap_file) # Read packets from pcap_file 
-    
-    
-    # Iterate the packets 
-    for packet in pkts:
-        
-        if packet.haslayer('HTTP'):
-            http_packet = str(packet[HTTP]) # Convert to string
-            headers_packet_dict = {}
-            try:
-                # Remove the request or response line and the payload of http packet
-                http_packet = http_packet[http_packet.index("\\r\\n")+4:http_packet.index("\\r\\n\\r\\n")+4]
-
-                # Create a dictionary { <header name> : <value> } 
-                headers_packet_dict = dict(re.findall(r"(?P<name>.*?): (?P<value>.*?)\\r\\n", http_packet))
-                num_of_http_pkts = num_of_http_pkts + 1
-            except: 
-                continue
-
-            # Update every header in the Header Counter
-            headers_counts.update(header for header in headers_packet_dict.keys())
-            # print(headers_counts.most_common())
-
-
-# Final print
-print('\n\n\033[1m{:40}{:20}{}\033[0m'.format('HEADER', 'COUNTS', 'KNOWN'))
-
-total_headers_known = 0
-for key,value in headers_counts.most_common():
-
-    headerIsKnown = False
-
-    if key in KNOWN_HEADERS:
-        headerIsKnown = True
-        total_headers_known = total_headers_known + 1
-
-    print ('{:34}{:10}{knwon:>20}'.format(key, value, knwon = 'yes' if headerIsKnown else 'no'))
-
-total_headers_found = len(headers_counts)
-print(f'\nTotal HTTP packets analized: {num_of_http_pkts}')
-print('Total HTTP headers = {} ({} knowns | {} unknowns)'.format(total_headers_found, total_headers_known, (total_headers_found - total_headers_known)))
+    total_headers_found = len(headers_counts)
+    print(f'\nTotal HTTP packets analized: {num_of_http_pkts}')
+    print('Total HTTP headers = {} ({} knowns | {} unknowns)'.format(total_headers_found, total_headers_known, (total_headers_found - total_headers_known)))
 
 ##############################################################################################################
 
+
+
+##############################################################################################################
+# PACKET_PARSER 
+
+def packet_parser(packet):
+    global num_of_http_pkts
+    if (packet.haslayer('HTTPRequest') or packet.haslayer('HTTPResponse')):
+        http_packet = str(packet[HTTP]) # Convert to string
+        headers_packet_dict = {}
+        try:
+            # Remove the request or response line and the payload of http packet
+            http_packet = http_packet[http_packet.index("\\r\\n")+4:http_packet.index("\\r\\n\\r\\n")+4]
+
+            # Create a dictionary { <header name> : <value> } 
+            headers_packet_dict = dict(re.findall(r"(?P<name>.*?): (?P<value>.*?)\\r\\n", http_packet))
+            
+            # Update every header in the Header Counter
+            headers_counts.update(header for header in headers_packet_dict.keys())
+
+            num_of_http_pkts = num_of_http_pkts + 1
+
+        except: 
+            return
+    return
+
+##############################################################################################################
+
+
+
+##############################################################################################################
+# MAIN
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-f", "--files", help="specify the pcap files", nargs='+')
+group.add_argument("-d", "--dir", help="specify the pcap directory", default=PCAPS_DIR)
+args = parser.parse_args()
+
+# Parsing arguments
+if(args.files):
+    files_list = args.files
+elif(args.dir):
+    if(not os.path.isdir(args.dir)):
+        print(f'*Error: {args.dir} is not a directory')
+        sys.exit(0)
+    PCAPS_DIR = Path(args.dir)
+    os.chdir(f'{PCAPS_DIR}')
+    files_list = glob.glob('*.pcap')
+    print(f"Looking for pcaps file in '{os.getcwd()}' directory")
+    
+# Check if pcap files was found
+if not len(files_list):
+    print('*Error: no pcap files was found')
+    exit()
+
+# Iterate the files
+for pcap_file in files_list:
+    if(not os.path.isfile(pcap_file)):
+        print(f'*Error: {pcap_file} is not a file')
+        continue
+        
+    print(f'Reading from {pcap_file}')
+    pkts = sniff(offline=pcap_file, prn=packet_parser, bfilter='tcp') # Read packets from pcap_file 
+
+# Final print
+if(len(headers_counts) != 0):
+    final_print()
+
+
+##############################################################################################################
